936fb1332f384e9d884667aae0028c9bd49f658c
insian <patrasoham1512@gmail.com>
2024-02-21 17:09:50 +0530

    initial commit

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = "ASIA5MQHNLBSSSCIZ5OL"
+  secret_key = "yh93btq7lc/CE5PObY1a1gQBYJcShan4yTLBhudI"
+  token = "FwoGZXIvYXdzEGoaDKYunZz3B8l/LAzroiKvAZbgjhjJPTZ2kjWSpRFX09UXkuIt2ZVnbimf5tBk7XX/cL2ESEXKZC9OII8xXp4+l//c/SF5vAXQFe875yqzdP1nyfUYENPbYeH9/w+GYhROpZZe+RHNqzcxKYsqAkzE/mCdv0aCf5kApmYMtz6/zvBO7d34cOzhR2g9QTXb/5+rJ8bncD5RgXWuFJgHnSSZ+FQFo8dbVTFKIiIRlS0b4tIWrp1PjEzNB6OLXUxrxq4ozuPWrgYyLckQrlQV61OdVjiLq+wzbloFJztdRODGgaY1eXw5SGcIH04D25JTOhHANBktkw=="
+}
